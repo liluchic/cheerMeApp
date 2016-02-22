aec6ce6c92211be53e53b1187369f706edff00ae
liat frenkel <liat frenkel>
2016-02-22 13:14:55 +0200

    first upload to git
    it's good so Icould share the project with my partner :P

---
+++ script.js
@@ -0,0 +1,40 @@
+
+<!-- Cheer Me App-->
+
+/*
+*********************
+=Welcome page
+**********************
+*/
+
+alert("Your improved life begins today!");
+
+
+/*
+*********************
+=form
+**********************
+*/
+<form>
+//<form action="action_page.php" method="get"> learn about PHP before handaling this part.
+//student's details
+ <fieldset>
+    <legend>Personal information:</legend>
+ 	 First name:<br>
+  	<input type="text" name="firstname"><br>
+  	Last name:<br>
+  	<input type="text" name="lastname">
+  	Email: <br>
+	<input type="text" name="E-mail">
+  </fieldset>
+ <fieldset>
+  	<legend>GOALS:</legend>
+<input type="radio" name="goal" value="get inspired" checked> inspiration coming your way!<br>
+  /*	<li>Get inspired</li> //quates that motivate you, onclick. Our current mission
+	<li>She codes academy, Course of study</li>
+	<li>Build a project</li>//for now we can link to the Projects website
+	<li>Find a job</li>
+	<li>Get ahead at your current job</li>
+*/
+  </fieldset>
+</form>
