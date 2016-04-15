3cac0d4c66c8c9ea9c2ca8d2feecb8336d78a042
liluchic <frenkelliat@gmail.com>
2016-04-15 17:53:50 +0300

    adding the diagram

--- cheerMeApp2.1/js/app.js
+++ cheerMeApp2.1/js/app.js
@@ -7,16 +7,16 @@ function leader(first, last, quate, img) {
     this.lastName = last;
     this.quate = quate;
     this.img = img;
-};
+}
 // creating new leaders with our constructor
-var orit = new leader("Orit", "Ophir", "'If it doesn't have a deadline - it's just dead'", "orit.jpg");
-var ruth = new leader("Ruth", "Polachek", "'blablabla'", "ruth.jpg");
-var luda = new leader("Luda", "Fux", "'blbabla'", "luda.jpg");
+var Orit = new leader("Orit", "Ophir", "'If it doesn't have a deadline - it's just dead'", "orit.jpg");
+var Ruth = new leader("Ruth", "Polachek", "'blablabla'", "ruth.jpg");
+var Luda = new leader("Luda", "Fux", "'blbabla'", "luda.jpg");
 
 //get random leader from array.
 
 GetLeaderFN = function() {
-	var leaders = [orit, ruth, luda];
+	var leaders = [Orit, Ruth, Luda];
 	var arrSize = leaders.length-1;
 	var rand = leaders[Math.round(Math.random() * arrSize)];
 	console.log(rand);
@@ -24,12 +24,15 @@ GetLeaderFN = function() {
 	
 };
 
-
+// get the woman's picture to go up the stairs
+$(document).ready(function(){
+	$("#upbox").animate({left: '+=150', bottom: '+=150'}, 4000);
+});
 
 //image appears with quate
 // var rand = Math.floor(Math.random()* $('.leaders').length); console.log(rand);
 /* 
-1. RANDOM  between 0- leaders.length
-2. var person = $(leaders[rand]);
-3. call methode - person.fadein(slow)
-*/
+ 1. RANDOM  between 0- leaders.length
+ 2. var person = $(leaders[rand]);
+ 3. call methode - person.fadein(slow)
+ */
