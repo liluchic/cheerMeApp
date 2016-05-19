b33b267a591ad91ab2a75fa4fb9d7230f40934cf
liluchic <frenkelliat@gmail.com>
2016-05-19 16:44:24 +0300

    leadersappear with the click of A button, to cheer you up

--- cheerMeApp2.1/js/app.js
+++ cheerMeApp2.1/js/app.js
@@ -20,36 +20,84 @@ GetLeaderFN = function() {
 	var arrSize = leaders.length-1;
 	var rand = leaders[Math.round(Math.random() * arrSize)];
 	console.log(rand);
-	document.getElementById("leaderAppear").innerHTML = rand.firstName + rand.lastName + rand.quate + rand.img;
-	
+  //maybe this row needs to change..?
+	document.getElementById("leaderAppear").innerHTML = rand.firstName + " " + rand.lastName + " " + rand.quate + " " + rand.img;
 }
-// 1. create extra checkboxes
-var nextCheckbox =
-    $(document.createElement('input')).attr({
-        id: 'lesson#' + lessonId,
-        name: 'lesson#' + lessoName,
-        value: 1,
-        type: 'checkbox'
-    });
+<!--*********-->
+var description = new Array("blank", "ruth", "lea", "orit", "rony", "luda");
+var pictures = new Array(5);
+var quates = new Array("blank", "ruthsays", "leasay", "oritsay", "ronysay", "ludasay");
+var counter = 0;
 
-// 2. change the lessons name and id of the new checkbox (which creates more checkboxes~!)
-var isItChecked = document.getElementById("lesson#1").checked;
-if (isItChecked === true) {
-    changeLessonName(); }
 
-// 3. when checkbox is clicked - next checkbox appears:
-document.getElementById("lesson1").addEventListener("click", nextCheckbox);
-checkbox[i].click =+ i++
+function initialize(){
+  // sets up the array with some startin values
+  // Andy Harris
+  pictures[0] = new Image(50, 50);
+  pictures[0].src = "blank.gif";
+  pictures[1] = new Image(50, 50);
+  pictures[1].src = "img/ruth.jpg";
+  pictures[2] = new Image(50, 50);
+  pictures[2].src = "img/lea.jpg";
+  pictures[3] = new Image(50, 50);
+  pictures[3].src = "img/orit.jpg";
+  pictures[4] = new Image(50, 50);
+  pictures[4].src = "img/rony.jpg";
+  pictures[5] = new Image(50, 50);
+  pictures[5].src = "img/luda.jpg";
+
+} // end initialize
+
+function upDate(){
+  //increments the counter and shows the next description
+  counter++;
+  if (counter > description.length){
+    counter = 0;
+  } // end if
+  document.imgDisplay.src = pictures[counter].src;
+  document.myForm.txtDescription.value = description[counter];
+  document.myForm.txtQuates.value = quates[counter];
+} // end upDate
+
+
+<!--*********-->
+
+// 1. create extra checkboxes and eventlistener that can make this happen
+
+var nextCheckbox = $(document).ready(function(){
+  $("document").createElement('input').attr({
+    id: 'lesson#' + lessonId,
+    name: 'lesson#' + lessoName,
+    value: 1,
+    type: 'checkbox'
+
+  });
+});
+
+//checkbox[i].click =+ i++
 //4. event: when checkbox clicked  -
-    var changeLessonName = function() {
-        for(i = 1; i < 33; i++) {
+var changeLessonName = function() {
+    for(i = 1; i < 33; i++) {
         lessoName += input.name[i] + "<br>";
         lessonId += input.id[i] + "<br>";
         lessonVal += input.value[i] + "<br>";
     } };
-console.log(changeLessonName);
-// $(document).ready(
+console.log(changeLessonName());
 
+// 2. CHECK IF CHECKBOX IS CHECKED
+// 3. IF IT IS - change the lessons name and id of the newly created checkbox
+var isItChecked = document.getElementById("lesson#").checked;
+if (isItChecked === true) {
+    changeLessonName(); }
+console.log(isItChecked);
+// 3. when checkbox is clicked - next checkbox appears:
+document.getElementById("lesson#1").addEventListener("click", nextCheckbox);
+
+
+// we want to push() all nextCheckboxes that are created into an array:
+var checkboxArray = [];
+var bb = checkboxArray.push(nextCheckbox);
+console.log(bb);
 //the addCheckbox function:
 /*
     function addCheckBox() {
@@ -70,7 +118,7 @@ var goUp = function(){
 
 //image appears with quate
 // var rand = Math.floor(Math.random()* $('.leaders').length); console.log(rand);
-/* 
+/*
  1. RANDOM  between 0- leaders.length
  2. var person = $(leaders[rand]);
  3. call methode - person.fadein(slow)
